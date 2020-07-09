25ebfe883fa43d167111f4343dcef5e24f4b3d73
Erling Stage <erling.stage@gmail.com>
2020-07-09 08:14:11 +0300

    use viz.js version 1.7.1

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-importScripts("tmp/viz-js.com/bower_components/viz.js/viz.js");
+importScripts("./modules/viz.js/viz.js");
 
 onmessage = function(e) {
   var result = Viz(e.data.src, e.data.options);
