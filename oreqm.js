eee43b7d46083125821c163c5902152073dfb81f
Erling Stage <erling.stage@gmail.com>
2020-07-11 20:38:22 +0300

    Format long search regex nicely in diagram legend

--- oreqm.js
+++ oreqm.js
@@ -212,10 +212,11 @@ function construct_graph_title() {
     }
 
     if (search_pattern.length) {
+      let pattern_string = search_pattern.trim().replace(/([^\n]{40,500}?\|)/g, '$1<BR ALIGN="LEFT"/>').replace(/\n/g, '<BR ALIGN="LEFT"/>')
       if (id_checkbox ) {
-        title += '  <tr><td>Search &lt;id&gt;</td><td colspan="2">{}<BR ALIGN="LEFT"/></td></tr>\n'.format(search_pattern.replace('\\', '\\\\'))
+        title += '  <tr><td>Search &lt;id&gt;</td><td colspan="2">{}<BR ALIGN="LEFT"/></td></tr>\n'.format(pattern_string.replace('\\', '\\\\'))
       } else {
-        title += '  <tr><td>Search text</td><td colspan="2">{}<BR ALIGN="LEFT"/></td></tr>\n'.format(search_pattern.replace('\\', '\\\\'))
+        title += '  <tr><td>Search text</td><td colspan="2">{}<BR ALIGN="LEFT"/></td></tr>\n'.format(pattern_string.replace('\\', '\\\\'))
       }
     }
 
