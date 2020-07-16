53d0e3906e02f46927ac40de63d594f9955da4e0
Erling Stage <erling.stage@elektrobit.com>
2020-07-16 16:58:23 +0300

    Simply local server scripts
    User no longer needs to know python version installed

---
+++ http_server.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+import sys
+
+if sys.version_info.major == 2:
+    import SimpleHTTPServer
+    print("Starting SimpleHTTPServer under python {}.{}".format(sys.version_info.major, sys.version_info.minor))
+    SimpleHTTPServer.test()
+else:
+    import http.server
+    print("Starting http.server under python {}.{}".format(sys.version_info.major, sys.version_info.minor))
+    http.server.test()
---
+++ http_server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python http_server.py
