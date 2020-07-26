475586683f278c942491d8ce9bcfce0b8b0a7e96
Erling Stage <erling.stage@gmail.com>
2020-07-26 14:36:31 +0300

    show error message if python is not available (no http server)

--- http_server.cmd
+++ http_server.cmd
@@ -1,2 +1,13 @@
-#!/bin/bash
-python http_server.py
+:: Check for Python Installation
+@python --version 2>NUL
+@if errorlevel 1 goto errorNoPython
+
+@echo Keep this window running for Visual ReqM2
+@python http_server.py
+goto :eof
+
+:errorNoPython
+@((((echo Python not installed. v2.7 or v3.x needed) & echo No http server started on localhost) & echo.) & echo Visual ReqM2 will not run from localhost:8000)|MSG *
+@echo.
+@echo Error^: Python not installed - Visual ReqM2 will not work
+
